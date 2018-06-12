23c780576462ade06d6e075fd06631730e5d5a80
Александр <mr.galaktika@gmail.com>
2018-06-12 15:16:18 +0600

    add from home

--- JS/JS.js
+++ JS/JS.js
@@ -1,23 +1,4 @@
 "use strict";
 
 
-function Animal(name){
-    this._name = name;
-
-    Animal.prototype.getName = function () {
-        alert("Animal name: " + this._name)
-    }
-}
-
-function Rabbit(name) {
-    Animal.apply(this,arguments);
-    Rabbit.prototype = Object.create(Animal.prototype);
-    Rabbit.prototype.move = function () {
-        alert("Rabbit " + this.getName() + "moves");
-    }
-}
-
-var rab = new Rabbit("Vasa");
-rab.move();
-
 
